ae4cc4d758238761c814243678a0e9b1e6575e11
Bhawana Negi <bhawananegi0224@gmail.com>
2024-05-09 13:41:33 +0200

    feat: new changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./home.css";
+import iskon1 from "../images/iskon1.jpg";
+import iskon2 from "../images/iskon2.jpg";
+import iskon3 from "../images/iskon3.jpg";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <><div>
         <section className="hero herobg">
-        <div class="overlay1"></div>
+          <div class="overlay1"></div>
           <div class="titlebox">
             <p class="lead1">Welcome to</p>
             <h1 class="tilSpan0">
@@ -14,10 +17,20 @@ class Home extends Component {
             </h1>
           </div>
           <div class="text-center bg-light">
-          <span class="tilSpan3">Open all days from </span>
+            <span class="tilSpan3">Open all days from </span>
           </div>
         </section>
+
       </div>
+      <h3>Upcoming Events</h3>
+      <div className="tilesContainer">
+          <div className="tilesSection">
+            <img src={iskon1}></img>
+          </div>
+          <div className="tilesSection">
+            <img src={iskon2}></img>
+          </div>
+        </div></>
     );
   }
 }
